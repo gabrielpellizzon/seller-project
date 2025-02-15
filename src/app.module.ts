41825985ed1da951ed20de4a68019f6cdc9e0075
gabrielpellizzon <gabrielpellizzon@gmail.com>
2025-02-15 19:12:06 -0300

    fet - redis implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
     MongooseModule.forRoot(process.env.DATABASE_URI),
     ProductsModule,
     OrdersModule,
