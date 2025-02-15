c99499d3d13dc6114438dce6cf06b50df2839bec
gabrielpellizzon <gabrielpellizzon@gmail.com>
2025-02-15 20:08:48 -0300

    fix - unused imports removed

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,21 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from 'src/schemas/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Product, ProductDocument } from 'src/schemas/product.schema';
-import { Model } from 'mongoose';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
 
 @Injectable()
 export class ProductsService {
-  private readonly CACHE_TTL = 30 * 60 * 1000;
-
   constructor(
     @InjectModel(Product.name)
     private readonly productModel: Model<ProductDocument>,
-    @Inject(CACHE_MANAGER)
-    private cacheService: Cache,
   ) {}
 
   async createProduct(
